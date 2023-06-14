4bed964c2b59b8dd3de687e7d9446ec9d84fd906
Evans <ngugie649@gmail.com>
2023-06-15 02:59:17 +0300

    This fixed the program to a proffesional way

--- Controller/phoneController.js
+++ Controller/phoneController.js
@@ -20,15 +20,20 @@ export const getContacts = async (req, res) => {
 export const getContact = async (req, res) => {
     try {
         const { id } = req.params;
-        let pool = await sql.connect(config.sql);
+        const pool = await sql.connect(config.sql);
+        const query = "SELECT * FROM contacts WHERE id = @id";
         const result = await pool.request()
             .input("id", sql.Int, id)
-            .query("select * from contacts where id = @id");
-        !result.recordset[0] ? res.status(404).json({ message: 'Contact not found' }):
+            .query(query);
+        
+        if (!result.recordset[0]) {
+            res.status(404).json({ message: 'Contact not found' });
+        } else {
             res.status(200).json(result.recordset);
+        }
     } catch (error) {
-        // console.log(error);
-        res.status(500).json({ error: 'An error occurred while retrieving Contact' });
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while retrieving the contact' });
     } finally {
         sql.close();
     }
@@ -37,65 +42,85 @@ export const getContact = async (req, res) => {
 // Creating a contact
 export const createContact = async (req, res) => {
     try {
-        const { full_name, mobile_no, work_no, email, address, group_id } = req.body;
-        let pool = await sql.connect(config.sql);
-        await pool.request()
-            .input("full_name", sql.VarChar, full_name) // Insert the description into the SQL query
-            .input("mobile_no", sql.VarChar, mobile_no)
-            .input("work_no", sql.VarChar, work_no)
-            .input("email", sql.VarChar, email)
-            .input("address", sql.VarChar, address)
-            .input("group_id", sql.VarChar, group_id)
-            .query("insert into contacts (full_name, mobile_no, work_no, email, address, group_id) values (@full_name, @mobile_no, @work_no, @email, @address, @group_id)"); // Execute the SQL query
-        res.status(201).json({ message: 'Contact created successfully' });
+      const { full_name, mobile_no, work_no, email, address, group_id } = req.body;
+  
+      const pool = await sql.connect(config.sql);
+  
+      const query = `
+        INSERT INTO contacts (full_name, mobile_no, work_no, email, address, group_id)
+        VALUES (@full_name, @mobile_no, @work_no, @email, @address, @group_id);
+      `;
+  
+      const request = pool.request()
+        .input("full_name", sql.VarChar, full_name)
+        .input("mobile_no", sql.VarChar, mobile_no)
+        .input("work_no", sql.VarChar, work_no)
+        .input("email", sql.VarChar, email)
+        .input("address", sql.VarChar, address)
+        .input("group_id", sql.VarChar, group_id);
+  
+      await request.query(query);
+  
+      res.status(201).json({ message: 'Contact created successfully' });
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred while creating the a contact' });
+      console.error(error);
+      res.status(500).json({ error: 'Failed to create the contact' });
     } finally {
-        sql.close();   // Close the SQL connection
-    };
-}
+      sql.close();
+    }
+  };
+  
 
 // Updating a contact
 export const updateContact = async (req, res) => {
     try {
-        const { id } = req.params;
-        const { full_name,  work_no, group_id } = req.body;
-        let pool = await sql.connect(config.sql);
-        await pool.request()
-            .input("id", sql.Int, id)
-            .input("full_name", sql.VarChar, full_name)
-            // .input("mobile_no", sql.VarChar, mobile_no)
-            .input("work_no", sql.VarChar, work_no)
-            // .input("email", sql.VarChar, email)
-            // .input("address", sql.VarChar, address)
-            .input("group_id", sql.VarChar, group_id)
-            .query("UPDATE contact SET full_name = @full_name,  work_no = @work_no, group_id = @group_id WHERE id = @id");
-        res.status(200).json({ message: 'Todo updated successfully' });
+      const { id } = req.params;
+      const { full_name, mobile_no, work_no, email, address, group_id } = req.body;
+  
+      let pool = await sql.connect(config.sql);
+      await pool
+        .request()
+        .input("id", sql.Int, id)
+        .input("full_name", sql.VarChar, full_name)
+        .input("mobile_no", sql.VarChar, mobile_no)
+        .input("work_no", sql.VarChar, work_no)
+        .input("email", sql.VarChar, email)
+        .input("address", sql.VarChar, address)
+        .input("group_id", sql.VarChar, group_id)
+        .query("UPDATE contacts SET full_name = @full_name, mobile_no = @mobile_no, work_no = @work_no, email = @email, address = @address, group_id = @group_id WHERE id = @id");
+  
+      res.status(200).json({ message: 'Contact updated successfully' });
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred while updating the contact' });
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while updating the contact' });
     } finally {
-        sql.close();
+      sql.close();
     }
-}
+  };
+  
 
-
-export const deleteContact = async (req, res) => {
-    let connection;
+//deleting a contact
+  export const deleteContact = async (req, res) => {
     try {
       const { id } = req.params;
   
-      connection = await sql.connect(config.sql);
-      const result = await connection.request().query(`
-        DELETE FROM Contacts
-        WHERE id  ${id};
-      `);
+      const pool = await sql.connect(config.sql);
+      const result = await pool
+        .request()
+        .input("id", sql.Int, id)
+        .query("DELETE FROM Contacts WHERE id = @id");
   
-      res.send(result);
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred while deleting the contact' });
-    } finally {
-      if (connection) {
-        connection.close();
+      if (result.rowsAffected[0] === 0) {
+        return res.status(404).json({ error: 'Contact not found' });
       }
+  
+      return res.status(200).json({ message: 'Contact deleted successfully' });
+    } catch (error) {
+      console.error('An error occurred while deleting the contact:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    } finally {
+      sql.close();
     }
   };
+  
+  
