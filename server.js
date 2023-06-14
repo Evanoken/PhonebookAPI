7a3d0602ab4d2f6b2469d5a270c26b54c44d3390
Lewis Muthaura <lewislemmu10@gmail.com>
2023-06-14 22:43:22 +0300

    Server,routes

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-import express from "express";
-import config from "./Db/config.js";
-import phoneRoutes from './Routes/phoneRoutes.js'
+import express from "express"; // Importing the Express.js framework
+import config from "./Db/config.js"; // Importing the configuration file
+import phoneRoutes from './Routes/phoneRoutes.js' // Importing the phoneRoutes module
 
-const app = express();
+const app = express(); // Creating an instance of the Express application
 
-// middlewares
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// Middlewares
+app.use(express.json()); // Parsing request bodies in JSON format
+app.use(express.urlencoded({extended: true})); // Parsing URL-encoded request bodies
 
-phoneRoutes(app);
+phoneRoutes(app); // Registering the phoneRoutes module with the app
 
+// Handling the root route
 app.get("/", (req, res) => {
     res.send("Welcome to the Matrix....!!!");
 });
 
+// Starting the server
 app.listen(config.port, () => {
     console.log(`Server is running on port ${config.url}`);
 });
